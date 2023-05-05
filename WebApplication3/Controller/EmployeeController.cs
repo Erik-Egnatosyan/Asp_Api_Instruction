9c3fb18fb2328b2528816e4d3657e981a99f4139
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-05-05 12:53:05 +0400

    Add entity

---
+++ WebApplication3/Controller/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _context.Employees.ToList();
+        }
+    }
+
+}
